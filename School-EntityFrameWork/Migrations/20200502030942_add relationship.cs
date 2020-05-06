40b1aaf2734b55d7d33420ef3a15a9e7f889164a
mahdis1360 <mahdismoazen@outlook.com>
2020-05-05 22:49:52 -0400

    entity

---
+++ School-EntityFrameWork/Migrations/20200502030942_add relationship.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace School_EntityFrameWork.Migrations
+{
+    public partial class addrelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "subjects",
+                columns: table => new
+                {
+                    subgect_id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Term = table.Column<int>(nullable: false),
+                    hours = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_subjects", x => x.subgect_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectStudent",
+                columns: table => new
+                {
+                    StudentId = table.Column<Guid>(nullable: false),
+                    SubjectId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectStudent", x => new { x.StudentId, x.SubjectId });
+                    table.ForeignKey(
+                        name: "FK_SubjectStudent_students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SubjectStudent_subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "subjects",
+                        principalColumn: "subgect_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectStudent_SubjectId",
+                table: "SubjectStudent",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubjectStudent");
+
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "subjects");
+        }
+    }
+}
---
+++ School-EntityFrameWork/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace School_EntityFrameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            schoolcontext context = new schoolcontext();
+            context.Database.EnsureCreated();
+        }
+
+        public class schoolcontext:DbContext
+        {
+            public DbSet<Student> students { get; set; }
+            public DbSet<Subject> subjects { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                             
+                optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-41TMA4UR;Initial Catalog=DB_School;Integrated Security=True");
+                base.OnConfiguring(optionsBuilder);
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<SubjectStudent>(entity =>
+                 {
+                    entity.HasKey(k=>
+                    new {k.StudentId,k.SubjectId });
+
+                     entity.HasOne(d => d.subjects)
+                     .WithMany(e => e.subjectStudents)
+                     .HasForeignKey(f => f.SubjectId);
+
+                     entity.HasOne(d => d.students)
+                     .WithMany(s => s.subjectStudents)
+                     .HasForeignKey(k => k.StudentId);
+                     
+
+                 });
+                base.OnModelCreating(modelBuilder);
+            }
+
+        }
+
+
+        public class Student
+         {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public DateTime Age { get; set; }
+            public ICollection<SubjectStudent> subjectStudents { get; set; }
+        }
+
+       public class SubjectStudent
+       {
+            public Guid StudentId { get; set; }
+            public Guid SubjectId { get; set; }
+            public Student students { get; set; }
+            public Subject subjects { get; set; }
+
+        }
+
+       public class Subject
+        {
+            [Key]
+            public Guid subgect_id { get; set; }
+            public string Name { get; set; }
+            public int Term { get; set; }
+            public int hours { get; set; }
+            public ICollection<SubjectStudent> subjectStudents { get; set; }
+        }
+    }
+
+   
+}
